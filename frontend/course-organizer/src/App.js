d78da960934d8bdec7ca9aecb9f953ef2b03713e
Recep Uysal <recep.uysal@ug.bilkent.edu.tr>
2024-02-20 02:01:31 +0300

    first commit

---
+++ frontend/course-organizer/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './Table';
+import EditableTable from './Table';
+
+function App() {
+  return (
+    <EditableTable></EditableTable>
+  );
+}
+
+export default App;
