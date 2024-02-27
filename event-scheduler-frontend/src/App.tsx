a88f8832f4d68421d12bb110e8addfdb4d8bc322
Recep Uysal <recep.uysal@ug.bilkent.edu.tr>
2024-02-27 09:38:16 +0300

    event-scheduler-front-initial

---
+++ event-scheduler-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainLayout from './MainLayout';
+
+function App() {
+  return (
+    <MainLayout></MainLayout>
+  );
+}
+
+export default App;
